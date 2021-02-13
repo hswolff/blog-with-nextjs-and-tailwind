bbff3ae3dca9e728361e40537975bb727a9f1101
Harry Wolff <hswolff@gmail.com>
2021-02-13 15:00:31 -0500

    get the raw functionality together!

---
+++ pages/blog/[slug].js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import { blogPosts } from '../../lib/data';
+
+export default function BlogPage({ title, date, content }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>{title}</h1>
+        <div>{content}</div>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps(context) {
+  console.log('hi!', context);
+  const { params } = context;
+  return {
+    props: blogPosts.find((item) => item.slug === params.slug),
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: blogPosts.map((item) => ({
+      params: {
+        slug: item.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { blogPosts } from '../lib/data';
 
 export default function Home() {
   return (
@@ -11,6 +13,20 @@ export default function Home() {
       <main>
         <h1>My Blog</h1>
       </main>
+
+      <div>
+        {blogPosts.map((item) => (
+          <div key={item.slug}>
+            <div>
+              <Link href={`/blog/${item.slug}`}>
+                <a>{item.title}</a>
+              </Link>
+            </div>
+            <div>{item.date.toString()}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
