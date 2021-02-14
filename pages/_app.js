01edba415062666518cf6a506c540a1242b99335
Harry Wolff <hswolff@gmail.com>
2021-02-13 21:44:18 -0500

    Make the overall layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
 import Link from 'next/link';
-
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto my-8 w-9/12">
+      <header>
+        <h1 className="text-6xl font-bold text-center">My Blog</h1>
+        <nav className="my-4">
+          <ul className="flex flex-row justify-center space-x-4">
+            <li>
+              <Link href="/">
+                <a className="">Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <Component {...pageProps} />
     </div>
   );
